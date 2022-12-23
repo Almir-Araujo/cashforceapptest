530f57df249d2f67d30694cc7787126b219fc772
Almir-Araujo <araujo.almir@gmail.com>
2022-12-23 20:56:46 -0300

    Iniciando Front End com VueJs

---
+++ testes-tecnicos/cashforceapp/frontend/cashforceApp/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+
+  <header>
+    <nav class="navbar bg-light">
+      <div class="container">
+        <a class="navbar-brand">
+          <img alt="Cash Force Logo" src="../logo-cashforce.svg" width="200" height="200" />
+        </a>
+      </div>
+    </nav>  
+  </header>
+
+</template>
