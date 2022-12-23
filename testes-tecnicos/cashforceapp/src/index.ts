bc63840b14706cf1ad7c6e5335de71f3d9250fb8
Almir-Araujo <araujo.almir@gmail.com>
2022-12-23 09:44:21 -0300

    Configurações iniciais

--- testes-tecnicos/cashforceapp/src/index.ts
+++ testes-tecnicos/cashforceapp/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 
 const app = express();
+const { PORT = 3001 } = process.env;
+
 app.use(express.json());
 // app.use(routes);
 // app.use(ErrorHandler.handle);
 
 export default app;
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
