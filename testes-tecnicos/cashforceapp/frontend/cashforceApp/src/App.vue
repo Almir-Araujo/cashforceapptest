70a50e5813408dbed9b4c2631f88ff43603622e5
Almir-Araujo <araujo.almir@gmail.com>
2022-12-23 21:21:59 -0300

    Commit para pausar atividade

--- testes-tecnicos/cashforceapp/frontend/cashforceApp/src/App.vue
+++ testes-tecnicos/cashforceapp/frontend/cashforceApp/src/App.vue
@@ -4,13 +4,17 @@
 <template>
 
   <header>
-    <nav class="navbar bg-light">
-      <div class="container">
-        <a class="navbar-brand">
-          <img alt="Cash Force Logo" src="../logo-cashforce.svg" width="200" height="200" />
+    <main class="d-flex flex-nowrap">
+      <h1 class="visually-hidden">Cash Force</h1>
+      <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-light">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-black text-decoration-none">
+          <svg class="bi pe-none me-2" width="40" height="32">
+            <use xlink:href="#bootstrap"></use>
+          </svg>
+          <span>Cash Force APP</span>
         </a>
       </div>
-    </nav>  
+    </main>
   </header>
 
 </template>
